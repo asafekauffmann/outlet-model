0d54df0e92d91a2ba59cdb39b7b7ac26b4dd15dc
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-26 09:32:38 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { Child01Component } from './child01/child01.component';
+import { Child02Component } from './child02/child02.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    Child01Component,
+    Child02Component
   ],
   imports: [
     BrowserModule,
