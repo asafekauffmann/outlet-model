569c323ff3c0be9eb4fa16ceb56394cf103c1aa6
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-26 10:59:36 -0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { Child02Component } from './child02/child02.component';
+import { Child01Component } from './child01/child01.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,9 +8,22 @@ import { SecondComponent } from './second/second.component';
 
 
 const routes: Routes = [
-   { path: 'first-component', component: FirstComponent },
+   { path: 'first-component', component: FirstComponent,
+      children: [
+         {
+            path: 'child01', component: Child01Component,
+         },
+         {
+            path: 'child02', component: Child02Component,
+         },
+      ],      
+   },
+
+
    { path: 'second-component', component: SecondComponent },
-   { path: '**', component: PagenotfoundComponent }
+
+   /* { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, */
+   /* { path: '**', component: PagenotfoundComponent } */
 
    ];
 
--- src/app/editgroceryitem/editgroceryitem.component.ts
+++ src/app/editgroceryitem/editgroceryitem.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
-  selector: 'app-editgroceryitem',
-  templateUrl: './editgroceryitem.component.html',
-  styleUrls: ['./editgroceryitem.component.sass']
+   selector: 'app-editgroceryitem',
+   templateUrl: './editgroceryitem.component.html',
+   styleUrls: ['./editgroceryitem.component.sass']
 })
 export class EditgroceryitemComponent implements OnInit {
+   
+   name: any;
 
-  constructor() { }
+   constructor(private route : ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      this.route.queryParams.subscribe(params => {
+         this.name = params['name'];
+      });
+   }
 
 }
